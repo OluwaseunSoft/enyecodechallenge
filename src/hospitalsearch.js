2283b1ce75f0301db151f35526e077d3e3b305c7
Oluwaseun <oluwaseunazure@yahoo.com>
2020-06-06 11:16:20 +0100

    hospital finder

--- src/hospitalsearch.js
+++ src/hospitalsearch.js
@@ -1,40 +1,63 @@
-import React, {useState} from "react";
-import {
-  FormControl,
-  InputLabel,
-  Input,
-  Button
-} from "@material-ui/core";
+import React from 'react';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+
+const useStyles = makeStyles((theme) => createStyles({
+    root: {
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+      width: 400,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  }),
+);
 
 export default function HospitalSearch()
 {
-    const [location, setLocation] = useState("Lagos");
+    const classes = useStyles();
 
-    function handleLocationChange(e){
-        setLocation(e.target.value);
-    }
-
-    return(
+    return (
         <div
         style={{
           display: "flex",
           justifyContent: "center",
-          margin: 20,
+          margin: 250,
           padding: 20
         }}>
-
-        <form style={{ width: "50%" }}>
-          <h1>Search The Nearest Hospital</h1>  
-          <FormControl margin="normal" fullWidth>           
-            <Input id="location" type="text"
-            name="location" 
-            value={location} 
-            onChange={handleLocationChange}/>
-          </FormControl>
-          <Button variant="contained" color="primary" size="medium">
-            Send
-          </Button>
-          </form>
+      <Paper component="form" className={classes.root}>
+        <IconButton className={classes.iconButton} aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <InputBase
+          className={classes.input}
+          placeholder="Search Hospitals Near You"
+          inputProps={{ 'aria-label': 'search hospitals near you' }}
+        />
+        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Divider className={classes.divider} orientation="vertical" />
+        <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+          <DirectionsIcon />
+        </IconButton>
+      </Paper>
       </div>
-    )
-}
+    );
+  }
